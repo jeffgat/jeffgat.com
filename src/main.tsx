85250d07bdba2ecc9a0a0af5c1a85a21eff3b8fc
jeffgat <jeff.gtbn@gmail.com>
2022-10-06 14:47:23 -0700

    Loading fonts properly

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import theme from './theme';
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/500.css'
+import '@fontsource/montserrat/700.css'
 
 ReactDOM.render(
   <React.StrictMode>
